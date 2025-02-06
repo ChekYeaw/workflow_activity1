e56c6cfb6567eb4d3b48bc32b52df294c30f5b10
ChekYeaw <ChekYeaw@gmail.com>
2025-02-07 00:37:42 +0800

    fix format

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "sctp-ce8-tfstate"
     region = "ap-southeast-1"
-    key = "chek-s3-tf-ci.tfstate" # must be different from other projects
+    key    = "chek-s3-tf-ci.tfstate" # must be different from other projects
   }
 }
